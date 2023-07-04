4542e816bc0f7bfc1f0e469b115f4123002e7f7d
stx4 <80523829+WindowsNTXP@users.noreply.github.com>
2023-07-04 20:37:17 +0100

    Add files via upload

---
+++ meowbutton.js
@@ -0,0 +1,11 @@
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+let button = document.getElementsByClassName("meowbutton");
+
+button.addEventListener("click"), function(){
+    document.getElementsByClassName("hiddenmeow").style.right = "-90vw"
+}
+
+const wait2s = async () => {
+    await delay(2000);
+}
